0bae77d408e5f735c67a55a6eb566a2bf37c2338
dcodeeer <62316561+dcodeeer@users.noreply.github.com>
2024-04-10 18:21:03 +0500

    fixes 1.0

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $last_donations = $db->query('SELECT *, (SELECT name FROM teams WHERE id = donat
 $top_teams = $db->query('SELECT t.*, COALESCE(SUM(tm.amount), 0) AS total_amount
 FROM teams t
 LEFT JOIN donations tm ON t.id = tm.team_id
-GROUP BY t.id;')->fetchAll(PDO::FETCH_ASSOC);
+GROUP BY t.id ORDER BY total_amount DESC;')->fetchAll(PDO::FETCH_ASSOC);
 
 function convertDate($timestamp) {
   $datetime = new DateTime($timestamp);
@@ -52,7 +52,8 @@ if ($last_donations) {
   <meta charset='utf-8' />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>Football</title>
+  <title>Футбольные донаты: поддержи свою команду и помоги нуждающимся</title>
+  <meta name='description' content='Наши футбольные донаты объединяют страсть к игре и добрые дела! Поддержите свою любимую команду и внесите вклад в благотворительность. Присоединяйтесь к нам и создавайте позитивные изменения через футбол!' />
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -63,7 +64,9 @@ if ($last_donations) {
 <body>
   <header>
     <div class='container'>
-      <div class='logo'>LOGO</div>
+      <div class='logo'>
+        <img src='/assets/logo.png' alt='logo' />
+      </div>
       <nav>
         <div class='item scroll-link' href='#about'>О сервисе</div>
         <div class='item scroll-link' href='#last'>Последние пожертвования</div>
@@ -89,21 +92,21 @@ if ($last_donations) {
 
     <section class='about container' id='about'>
       <div class='title'>О сервисе</div>
-      <div class='text'>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца.</div>
+      <div class='text'>Добро пожаловать на наш сайт, где страсть к футболу превращается в благотворительность! Здесь каждый донат, сделанный фанатами в поддержку своей команды, становится шагом к помощи нуждающимся. Присоединяйтесь к нашему сообществу футбольных энтузиастов и вместе мы сможем изменить мир, делая добрые дела через любовь к игре!</div>
     </section>
 
     <section class='second container'>
       <div class='item'>
-        <div class='title'>Why do we use it?</div>
-        <div class='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+        <div class='title'>1</div>
+        <div class='description'>Мы сотрудничаем с надежными и проверенными благотворительными организациями, действующими в области помощи нуждающимся, детям, животным и другим социальным группам.</div>
       </div>
       <div class='item'>
-        <div class='title'>Why do we use it?</div>
-        <div class='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+        <div class='title'>2</div>
+        <div class='description'>Мы обеспечиваем прозрачность и отчетность в отношении использования средств, пожертвованных через наш сайт. Вы сможете получить отчет о том, как были использованы ваши пожертвования.</div>
       </div>
       <div class='item'>
-        <div class='title'>Why do we use it?</div>
-        <div class='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</div>
+        <div class='title'>3</div>
+        <div class='description'>Мы регулярно обновляем информацию о благотворительных проектах, на которые направляются пожертвования с нашего сайта. Вы сможете узнать о новых проектах, успехах и результатах благотворительной деятельности, поддерживаемой фанатами футбола через нашу платформу.</div>
       </div>
     </section>
 
@@ -111,11 +114,12 @@ if ($last_donations) {
       <div class='container'>
         <div class='title'>Пожертвовать</div>
         <form class='form' method='POST' action='payment.php'>
-          <select name='team'>
+          <select name='team' required>
+            <option value=''>Выберите команду</option>
             <?php
             $i = 0;
             foreach ($teams as $team) : ?>
-            <option <?php ($i == 0) ? 'selected' : '' ?> value='<?php echo $team['id']; ?>'><?php echo $team['name']; ?></option>
+            <option value='<?php echo $team['id']; ?>'><?php echo $team['name']; ?></option>
             <?php
             $i++;
             endforeach; ?>
@@ -192,7 +196,7 @@ if ($last_donations) {
 
   <footer>
     <div class='container'>
-      <div class='copyright'>&copy; lovemyteam.ru</div>
+      <div class='copyright'>&copy; lovemyteam.ru ИНН 053601714416 Дибиров С.М. </div>
       <div class='socials'>
         <a href='#' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z"/><path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z"/></svg></a>
         <a href='#' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M414.73 97.1A222.14 222.14 0 00256.94 32C134 32 33.92 131.58 33.87 254a220.61 220.61 0 0029.78 111L32 480l118.25-30.87a223.63 223.63 0 00106.6 27h.09c122.93 0 223-99.59 223.06-222A220.18 220.18 0 00414.73 97.1zM256.94 438.66h-.08a185.75 185.75 0 01-94.36-25.72l-6.77-4-70.17 18.32 18.73-68.09-4.41-7A183.46 183.46 0 0171.53 254c0-101.73 83.21-184.5 185.48-184.5a185 185 0 01185.33 184.64c-.04 101.74-83.21 184.52-185.4 184.52zm101.69-138.19c-5.57-2.78-33-16.2-38.08-18.05s-8.83-2.78-12.54 2.78-14.4 18-17.65 21.75-6.5 4.16-12.07 1.38-23.54-8.63-44.83-27.53c-16.57-14.71-27.75-32.87-31-38.42s-.35-8.56 2.44-11.32c2.51-2.49 5.57-6.48 8.36-9.72s3.72-5.56 5.57-9.26.93-6.94-.46-9.71-12.54-30.08-17.18-41.19c-4.53-10.82-9.12-9.35-12.54-9.52-3.25-.16-7-.2-10.69-.2a20.53 20.53 0 00-14.86 6.94c-5.11 5.56-19.51 19-19.51 46.28s20 53.68 22.76 57.38 39.3 59.73 95.21 83.76a323.11 323.11 0 0031.78 11.68c13.35 4.22 25.5 3.63 35.1 2.2 10.71-1.59 33-13.42 37.63-26.38s4.64-24.06 3.25-26.37-5.11-3.71-10.69-6.48z" fill-rule="evenodd"/></svg></a>
